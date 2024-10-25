599cac261b656ec06dc52e7f1bc9a41d23cca826
usama <u_hameed@apple.com>
2024-10-25 16:48:17 -0700

    Revert "Enable debug info serialization by default"
    This reverts commit 384877fc1dffc3ed8aa7a7c148808106d9f9e39b.

--- include/swift/Serialization/SerializationOptions.h
+++ include/swift/Serialization/SerializationOptions.h
@@ -151,7 +151,7 @@ namespace swift {
 
     bool AutolinkForceLoad = false;
     bool SerializeAllSIL = false;
-    bool SerializeDebugInfoSIL = true;
+    bool SerializeDebugInfoSIL = false;
     bool SerializeOptionsForDebugging = false;
     bool IsSIB = false;
     bool DisableCrossModuleIncrementalInfo = false;
